15b326e9435ac492e74b2bd1a26eba1e77d1a34d
CollinTheDev <collin.entringer@gmail.com>
2024-06-24 23:40:49 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
 Config = {}
 
-Config.ApiUrl = "https://beta.communitycad.app/api/v1/emergency/attach_unit"
-Config.ApiKey = "iSbTORHamVRmkF8sxT6EpxsoqfcRkeURtg32wCdEdsfUUgwymqerkC7s2UladKeH"
+Config.ApiUrl = "https://COMMUNITY.communitycad.app/api/v1/emergency/attach_unit" -- Replace COMMUNITY with the begining of your CAD URL
+Config.ApiKey = "API_KEY" -- Replace API_KEY with the API key you generated in the CAD settings
